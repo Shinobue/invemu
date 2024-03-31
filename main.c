73ceadea780530d05e9b3353c27d8c781ac21135
Axel <14004169+Shimedaiko@users.noreply.github.com>
2024-03-31 13:27:26 +0200

    Initial project version

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+int main(){
+
+    int mByte = 0xA57;
+    unsigned char myChar = 153;
+
+    printf("The value of myChar is %d and the character is %c\n", myChar, myChar);
+
+    printf("%d\n", mByte);
+
+}
+
+
+
